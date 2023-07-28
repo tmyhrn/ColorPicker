c0abe682ca169aca2a19faafc607044c94b2cb6e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-07-28 06:47:51 +0000

    3-1修正

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ const colorBg = () => {
   document.body.style.backgroundColor = color.value;
   
   if (color.value === '#ffffff') {
-    text.textContent = 'カラーコード: ${color.value} (white)';
-  } else if (color.value === '##000000') {
-    text.textContent = 'カラーコード: ${color.value} (black)';
+    text.textContent = `カラーコード: ${color.value} (white)`;
+  } else if (color.value === '#000000') {
+    text.textContent = `カラーコード: ${color.value} (black)`;
   } else {
-    text.textContent = 'カラーコード: ${color.value}';
+    text.textContent = `カラーコード: ${color.value}`;
   }
 }
 
